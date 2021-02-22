353c901bbd82996e800299a504964542fbff7622
Yaroslav Senishyn <fastindian84@gmail.com>
2021-02-22 23:31:09 +0200

    Initial commit

---
+++ lib/lexorank_ex.ex
@@ -0,0 +1,18 @@
+defmodule LexorankEx do
+  @moduledoc """
+  Documentation for `LexorankEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LexorankEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lexorank_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule LexorankExTest do
+  use ExUnit.Case
+  doctest LexorankEx
+
+  test "greets the world" do
+    assert LexorankEx.hello() == :world
+  end
+end
