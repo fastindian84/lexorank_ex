7722d99cb05cc53d0834a8fc0e714feab99e34a6
Yaroslav Senishyn <fastindian84@users.noreply.github.com>
2021-03-17 23:16:46 +0200

    Remove extra packages (#1)

--- mix.exs
+++ mix.exs
@@ -23,9 +23,7 @@ defmodule LexorankEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", runtime: false},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.24.0", only: :dev, runtime: false},
     ]
   end
 
